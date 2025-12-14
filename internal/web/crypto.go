852398957cfa596b0bb8fd5255c72c3a2a8cb17b
Dan Kirkwood <dangogh@gmail.com>
2025-12-14 15:08:24 -0800

    Add shared crypto token generation

---
+++ internal/web/crypto.go
@@ -0,0 +1,15 @@
+package web
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+// generateToken generates a cryptographically secure random token
+func generateToken() (string, error) {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
